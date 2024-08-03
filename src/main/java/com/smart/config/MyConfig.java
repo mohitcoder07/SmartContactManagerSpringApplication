70d68a1f2be3ed7a66a27dcd8300d9e5f2a4d360
Mohit Saini <mohitsaini14937@gmail.com>
2024-08-03 11:03:34 +0530

    NEW CODE

---
+++ src/main/java/com/smart/config/MyConfig.java
@@ -0,0 +1,55 @@
+package com.smart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class MyConfig extends 	WebSecurityConfiguration {
+	
+	@Bean
+	public UserDetailsService getUserDetailsService() {
+		return new UserDetailsServiceImpl();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		
+		return new  BCryptPasswordEncoder();
+	}
+	
+	@Bean
+   public DaoAuthenticationProvider authenticationProvider() {
+	   
+	   DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+	   daoAuthenticationProvider.setUserDetailsService(this. getUserDetailsService());
+	   daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+	   
+	   return daoAuthenticationProvider;
+   }
+	
+	
+	//configure methods.....
+	
+	
+	 
+	//@override
+	protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+		auth.authenticationProvider(authenticationProvider());
+		
+	}
+	//@override
+	protected void configure(HttpSecurity http)throws Exception{
+		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+		.antMatchers("/user/**").hasRole("user")
+		.antMatchers(""/**").permitAll().and().formlogin().and().csrf().disable();
+
+  }
+}
